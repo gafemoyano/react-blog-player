7def7b3a2447cde3f36290b38c61b2a762658b13
Felipe Moyano <gafemoyano@gmail.com>
2017-01-28 17:13:03 -0500

    Refactor player internals

--- src/OnsitePlayer.js
+++ src/OnsitePlayer.js
@@ -3,13 +3,12 @@ import ChangelogAudio from "./Audio";
 import Episode from './episode'
 import axios from 'axios';
 import cx from 'classnames'
-import PodcastCoverArt from './assets/img/podcast-cover-art.svg'
+import SampleAudio from './assets/audio/Bloodflood.mp3'
+import SampleArt from './assets/audio/an-awesome-wave.jpg'
+import DefaultArt from './assets/img/podcast-cover-art.svg'
 import PodcastArrow from './assets/img/podcast-arrow.svg'
 
 class OnsitePlayer extends Component {
-  props = {
-
-  }
   state = {
     audio: new ChangelogAudio (),
     audioLoaded: false,
@@ -17,11 +16,16 @@ class OnsitePlayer extends Component {
     isHidden: false,
     isPaused: true,
     isPlaying: false,
-    episode: {
-      art: PodcastCoverArt,
+    episode: new Episode({
+      prev: null,
+      next:null,
+      art_url: SampleArt,
+      audio_url: SampleAudio,
+      podcast: 'Week 1',
+      duration: 3600,
       title: 'An awesome wave',
       blogTitle: 'Episode 44: Whatever',
-    },
+    }),
     detailsLoaded: false,
   }
   componentDidMount() {
@@ -35,8 +39,7 @@ class OnsitePlayer extends Component {
   }
   pause() {
     console.log('pause')
-    this.audio.pause();
-    this.playButton.addClass("is-paused").removeClass("is-playing is-loading");
+    // this.audio.pause();
   }
   togglePlayPause() {
     console.log('togglePlayPause')
@@ -52,8 +55,12 @@ class OnsitePlayer extends Component {
   }
 
   hide = () => {
-    this.setState({isHidden: true})
+
   }
+  togglePlayer = () => {
+    this.setState({isHidden: !this.state.isHidden})
+  }
+
   loadEpisode(event) {
     event.preventDefault()
     if(this.canPlay()) {
@@ -151,54 +158,52 @@ class OnsitePlayer extends Component {
       {'podcast-player--is-hidden': this.state.isHidden},
     )
     return (
-<div>
-  <div><button onClick={this.hide}>Toggle Player</button></div>
-  <div className="player-container" ref={(node) =>  {this.container = node}}>
-    <div id="player" ref={(node) => this.player = node}>
-      <figure className={playerClasses}>
-        <PlayerNav
-          hide={this.hide}
-          close={this.close}
-        />
-        <img
-          className="podcast-player_art js-player-art"
-          role="presentation"
-          src={this.state.episode.art}
-        />
-        <PlayerDetails
-          title={this.state.episode.title}
-          blogTitle={this.state.episode.blogTitle} />
-        <PlayerButtons isPaused={this.state.isPaused} />
-        <PlayerSlider
-          episode={this.state.episode}
-        />
-      </figure>
-    </div>
-  </div>
-</div>
+      <div>
+        <div>
+          <button onClick={this.hide}>Toggle Player</button>
+        </div>
+        <div className="player-container" ref={(node) =>  {this.container = node}}>
+          <div id="player" ref={(node) => this.player = node}>
+            <figure className={playerClasses}>
+              <PlayerNav
+                togglePlayer={this.togglePlayer}
+                close={this.close}
+              />
+              <PlayerArt source={this.state.episode.art()} />
+              <PlayerDetails
+                title={this.state.episode.title()}
+                blogTitle={this.state.episode.blogTitle} />
+              <PlayerButtons isPaused={this.state.isPaused} />
+              <PlayerSlider
+                episode={this.state.episode}
+              />
+            </figure>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-const PlayerNav = ({hide, close}) => {
+const PlayerNav = ({togglePlayer, close}) => {
   return(
     <nav className="podcast-player_nav">
       <button
         className="podcast-player_nav_button podcast-player_nav_button--hide js-player-hide"
-        title="Hide Player"
-        onClick={() => hide}>
+        title="Toggle Player"
+        onClick={togglePlayer}>
         </button>
       <button
         className="podcast-player_nav_button podcast-player_nav_button--close js-player-close"
         title="Close Player"
-        onClick={() =>  close}>
+        onClick={close}>
       </button>
     </nav>
   )
 }
 
 PlayerNav.propTypes = {
-  hide: PropTypes.func.isRequired,
+  togglePlayer: PropTypes.func.isRequired,
   close: PropTypes.func.isRequired,
 }
 
@@ -232,29 +237,24 @@ const PlayerButtons = ({isPaused, playEpisode}) => {
     <div className="podcast-player_buttons">
       <button
         className="podcast-player-button podcast-player-button--episode js-player-prev-button"
-        title="Listen to the previous episode"
-        ref={(node) =>  {this.prevButton = node}}>
+        title="Listen to the previous episode">
         <span
-          className="js-player-prev-number"
-          ref={(node) =>  {this.prevNumber = node}}>
+          className="js-player-prev-number">
         </span>
         <img src={PodcastArrow} alt="Previous episode"/>
       </button>
       <button
         className="podcast-player-button podcast-player-button--back15 js-player-back-button"
-        title="Seek back 15 seconds"
-        ref={(node) =>  {this.backButton = node}}>
+        title="Seek back 15 seconds">
       </button>
 
       <button
-        className={playButtonClasses}
-        ref={(node) =>  {this.playButton = node}}>
+        className={playButtonClasses}>
       </button>
 
       <button
         className="podcast-player-button podcast-player-button--forward15 js-player-forward-button"
-        title="Seek forward 15 seconds"
-        ref={(node) =>  {this.forwardButton = node}}>
+        title="Seek forward 15 seconds">
       </button>
       <button className="podcast-player-button podcast-player-button--episode js-player-next-button" title="Listen to the next episode">
         <img
@@ -277,10 +277,22 @@ PlayerButtons.propTypes = {
   blogTitle: PropTypes.string,
 }
 
+const PlayerArt = ({source}) => (
+  <img
+    className="podcast-player_art js-player-art"
+    role="presentation"
+    src={source || DefaultArt}
+  />
+)
+
+PlayerArt.propTypes = {
+  source: PropTypes.string,
+}
+
 const PlayerSlider = ({currentSeek, episode, handleScrubberInput, handleScrubberChange}) => {
   const seek = Math.round(currentSeek || 0)
   const percentComplete = seek / episode.duration() * 100
-
+  const current = Episode.formatTime(currentSeek)
   return (
     <form className="podcast-player_slider">
       <div className="range-slider">
@@ -306,7 +318,7 @@ const PlayerSlider = ({currentSeek, episode, handleScrubberInput, handleScrubber
         </div>
         <span className="range-slider_below"> <span className="js-player-title"></span>
           <output>
-            <b className="js-player-current">0:00</b> / <span className="js-player-duration" ref={(node) =>  {this.duration = node}}>0:00</span>
+            <b className="js-player-current">{current}</b> / <span className="js-player-duration">{Episode.formatTime(episode.duration())}</span>
           </output>
         </span>
       </div>
